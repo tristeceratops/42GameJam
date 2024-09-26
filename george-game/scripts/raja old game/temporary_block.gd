a15f866aac50c8676b93d7881058bd7ec8cddc59
Eric <eric.woillard@gmail.com>
2024-09-26 15:24:52 +0200

    merge in coming

--- george-game/scripts/raja old game/temporary_block.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-var is_platform = true
-
-func _physics_process(delta: float) -> void:
-	$"keaboard platform".disabled = is_platform
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	is_platform = false
