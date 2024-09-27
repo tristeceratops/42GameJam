82e4498a72bdce77b898aee5f33cc84c94998324
laichoun <aichouniasma@gmail.com>
2024-09-27 14:57:08 +0200

    Final version_final_victory

--- george-game/testing stages/boss_level/victory/victory_scene.gd
+++ george-game/testing stages/boss_level/victory/victory_scene.gd
@@ -1,4 +1,4 @@
 extends CanvasLayer
 
 func _on_menu_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://recources/starting screen.tscn")
+	get_tree().change_scene_to_file("res://recource/starting screen.tscn")
